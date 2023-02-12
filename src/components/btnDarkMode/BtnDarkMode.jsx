3d2fe3a10baeec478d6d405a6dbc39032aeb78c5
Vadym Liumanov <vliumanov@gmail.com>
2023-02-12 12:01:26 +0300

    btnDarkMode was refactored

--- src/components/btnDarkMode/BtnDarkMode.jsx
+++ src/components/btnDarkMode/BtnDarkMode.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect } from "react"
 
 import { useLocalStorage } from "../../utils/useLocalStorage"
 
@@ -9,12 +9,10 @@ import moon from "./moon.svg"
 import detectDarkMode from "../../utils/detectDarkMode"
 
 const BtnDarkMode = () => {
-  // const [mode, setMode] = useState('light')
 
   const initialModeValue = detectDarkMode()
 
   const [mode, setMode] = useLocalStorage('darkMode', initialModeValue)
-  const btnRef = useRef(null)
 
   const toggleDarkMode = () => {
     setMode((prevValue) => prevValue === 'light' ? 'dark' : 'light')
@@ -23,19 +21,16 @@ const BtnDarkMode = () => {
   useEffect(() => {
     if (mode === 'dark') {
       document.body.classList.add('dark')
-      btnRef.current.classList.add('dark-mode-btn--active')
     } else {
       document.body.classList.remove('dark')
-      btnRef.current.classList.remove('dark-mode-btn--active')
     }
   }, [mode])
 
   /* Этот useEffect будет выполнен единожды.
   Он повесит прослушку на динамическое изменение темы в системе - как только 
-  в системе поменяется тема, она автоматически сменится в приложении 
-  
-  TODO
-  Убрать мусор в виде прослушки при анмаунте приложения 
+  в системе поменяется тема, она автоматически сменится в приложении.
+
+  Через return подчищаем мусор в виде removeEventListener при анмаунте компонента
   */
 
   useEffect(() => {
@@ -45,10 +40,20 @@ const BtnDarkMode = () => {
         const newColorScheme = event.matches ? "dark" : "light"
         setMode(newColorScheme)
       })
+
+    return window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .removeEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light"
+        setMode(newColorScheme)
+      })
   }, [setMode])
 
+  const normalStyle = "dark-mode-btn"
+  const activeStyle = "dark-mode-btn dark-mode-btn--active"
+
   return (
-    <button className="dark-mode-btn" onClick={toggleDarkMode} ref={btnRef}>
+    <button className={(mode === 'dark') ? activeStyle : normalStyle} onClick={toggleDarkMode} >
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
