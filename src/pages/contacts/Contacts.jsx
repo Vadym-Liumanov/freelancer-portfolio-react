57738bb1f8b1ec02c017d9ca406dcba449941f34
Vadym Liumanov <vliumanov@gmail.com>
2023-02-14 18:42:46 +0300

    Content editing stage. Contacts page was edited

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -6,7 +6,6 @@ const Contacts = () => {
             <main className="section">
                 <div className="container">
                     <h1 className="title-1">Contacts</h1>
-
                     <ul className="content-list">
                         <li className="content-list__item">
                             <h2 className="title-2">Location</h2>
@@ -18,10 +17,9 @@ const Contacts = () => {
                         </li>
                         <li className="content-list__item">
                             <h2 className="title-2">Email</h2>
-                            <p><a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a></p>
+                            <p><a href="mailto:webdev@gmail.com">webdev@gmail.com</a></p>
                         </li>
                     </ul>
-
                 </div>
             </main>
         </>
