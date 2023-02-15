0f3060631630a6f558f7a706d880c2537fd50f0d
Vadym Liumanov <vliumanov@gmail.com>
2023-02-15 10:22:12 +0300

    Small changes about content in header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="header">
       <div className="header__wrapper">
         <h1 className="header__title">
-          <strong>Hi, my name is <em>Vadim</em></strong><br />
+          <strong>Hi, my name is <em>Vadym</em></strong><br />
           a frontend developer
         </h1>
         <div className="header__text">
