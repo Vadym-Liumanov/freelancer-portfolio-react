e80fce1058e9a77e35c5de26dbd1bc9a7533aaee
Vadym Liumanov <vliumanov@gmail.com>
2023-03-31 17:06:36 +0300

    Projects data was actualized (photos and text)

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,7 +1,9 @@
-import {
-  img01 ,img02, img03, img04, img05, img06, /* img07, */
-  imgBig01, imgBig02, imgBig03, imgBig04, imgBig05, imgBig06, /* imgBig07 */
-} from "../img/projects"
+import { img01, img09, img08, imgBig01, imgBig09, imgBig08 } from "../img/projects"
+
+// import {
+//   img01, img02, img03, img04, img05, img06, /* img07, */ img09,
+//   imgBig01, imgBig02, imgBig03, imgBig04, imgBig05, imgBig06, /* imgBig07 */ imgBig09
+// } from "../img/projects"
 
 const projectsList = [
   {
@@ -15,55 +17,75 @@ const projectsList = [
     }
   },
   {
-    id: '2',
-    title: 'Video service',
-    skills: 'React, PHP, MySql',
-    gitHubLink: 'https://github.com',
+    id: '8',
+    title: 'Resume',
+    skills: 'JavaScript, HTML, CSS',
+    gitHubLink: 'https://github.com/Vadym-Liumanov/resume-liumanov',
     img: {
-      small: img02,
-      big: imgBig02
+      small: img08,
+      big: imgBig08
     }
   },
   {
-    id: '3',
-    title: 'Video portal',
-    skills: 'Vue JS, Node.js, MongoDB',
-    gitHubLink: 'https://github.com',
+    id: '9',
+    title: 'Social network',
+    skills: 'Typescript, HTML, CSS',
+    gitHubLink: 'https://github.com/Vadym-Liumanov/social-network',
     img: {
-      small: img03,
-      big: imgBig03
+      small: img09,
+      big: imgBig09
     }
   },
-  {
-    id: '4',
-    title: 'Dating app',
-    skills: 'React Native',
-    gitHubLink: null,
-    img: {
-      small: img04,
-      big: imgBig04
-    }
-  },
-  {
-    id: '5',
-    title: 'Landing',
-    skills: 'HTML, SCSS, JS',
-    gitHubLink: null,
-    img: {
-      small: img05,
-      big: imgBig05
-    }
-  },
-  {
-    id: '6',
-    title: 'Gaming community',
-    skills: 'React, PHP, MySql',
-    gitHubLink: null,
-    img: {
-      small: img06,
-      big: imgBig06
-    }
-  },
+  // {
+  //   id: '2',
+  //   title: 'Video service',
+  //   skills: 'React, PHP, MySql',
+  //   gitHubLink: 'https://github.com',
+  //   img: {
+  //     small: img02,
+  //     big: imgBig02
+  //   }
+  // },
+  // {
+  //   id: '3',
+  //   title: 'Video portal',
+  //   skills: 'Vue JS, Node.js, MongoDB',
+  //   gitHubLink: 'https://github.com',
+  //   img: {
+  //     small: img03,
+  //     big: imgBig03
+  //   }
+  // },
+  // {
+  //   id: '4',
+  //   title: 'Dating app',
+  //   skills: 'React Native',
+  //   gitHubLink: null,
+  //   img: {
+  //     small: img04,
+  //     big: imgBig04
+  //   }
+  // },
+  // {
+  //   id: '5',
+  //   title: 'Landing',
+  //   skills: 'HTML, SCSS, JS',
+  //   gitHubLink: null,
+  //   img: {
+  //     small: img05,
+  //     big: imgBig05
+  //   }
+  // },
+  // {
+  //   id: '6',
+  //   title: 'Gaming community',
+  //   skills: 'React, PHP, MySql',
+  //   gitHubLink: null,
+  //   img: {
+  //     small: img06,
+  //     big: imgBig06
+  //   }
+  // },
   // {
   //   id: '7',
   //   title: 'Gaming streaming portal',
@@ -74,7 +96,7 @@ const projectsList = [
   //     big: imgBig07
   //   }
   // }
-  
+
 ]
 
 export { projectsList }
--- src/img/projects/index.js
+++ src/img/projects/index.js
@@ -1,4 +1,14 @@
+// Portfolio
 export {default as img01} from "./01.jpg"
+export {default as imgBig01} from "./01-big.jpg"
+// Resume
+export {default as img08} from "./08.jpg"
+export {default as imgBig08} from "./08-big.jpg"
+// Social network
+export {default as img09} from "./09.jpg"
+export {default as imgBig09} from "./09-big.jpg"
+
+// Unused img
 export {default as img02} from "./02.jpg"
 export {default as img03} from "./03.jpg"
 export {default as img04} from "./04.jpg"
@@ -6,7 +16,7 @@ export {default as img05} from "./05.jpg"
 export {default as img06} from "./06.jpg"
 export {default as img07} from "./07.jpg"
 
-export {default as imgBig01} from "./01-big.jpg"
+
 export {default as imgBig02} from "./02-big.jpg"
 export {default as imgBig03} from "./03-big.jpg"
 export {default as imgBig04} from "./04-big.jpg"
