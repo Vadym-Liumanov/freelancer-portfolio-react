bd7890ab500a935def984d269258d7a499798dcb
Vadym <vliumanov@gmail.com>
2023-07-22 19:45:14 +0300

    Adaptive design for Contacts page was completed

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,28 +1,37 @@
 import React from "react"
 
+import './styles.css'
+
 const Contacts = () => {
     return (
-        <>
-            <main className="section">
-                <div className="container">
-                    <h1 className="title-1">Contacts</h1>
-                    <ul className="content-list">
-                        <li className="content-list__item">
-                            <h2 className="title-2">Location</h2>
-                            <p>Voronezh, Russia</p>
+        <section className="contacts">
+            <div className="container">
+                <div className="contacts__wrapper">
+                    <h1 className="contacts__title title-1">Contacts</h1>
+                    <ul className="contacts__list">
+                        {/* Location исключен из контактов добавлением класса "none" */}
+                        <li className="contacts__item contacts__location none">
+                            <h2 className="contacts__subtitle">Location</h2>
+                            <p className="contacts__text">Voronezh, Russia</p>
                         </li>
-                        <li className="content-list__item">
-                            <h2 className="title-2">Phone</h2>
-                            <p><a href="tel:+79202049920">+7 (920) 204-99-20</a></p>
+
+                        <li className="contacts__item contacts__phone">
+                            <h2 className="contacts__subtitle">Phone</h2>
+                            <p className="contacts__text">
+                                <a href="tel:+79202049920" className="contacts__link">+7 (920) 204-99-20</a>
+                            </p>
                         </li>
-                        <li className="content-list__item">
-                            <h2 className="title-2">Email</h2>
-                            <p><a href="mailto:vliumanov@gmail.com">vliumanov@gmail.com</a></p>
+
+                        <li className="contacts__item contacts__email">
+                            <h2 className="contacts__subtitle">Email</h2>
+                            <p className="contacts__text">
+                                <a href="mailto:vliumanov@gmail.com" className="contacts__link">vliumanov@gmail.com</a>
+                            </p>
                         </li>
                     </ul>
                 </div>
-            </main>
-        </>
+            </div>
+        </section>
     )
 }
 
